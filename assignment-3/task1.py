198f51be23941692d3d7cd3fd3c715ffb7f372f7
Edvard Bakken <edvard.vb@live.no>
2016-09-29 11:35:39 +0200

    Add comments

--- assignment-3/task1.py
+++ assignment-3/task1.py
@@ -5,22 +5,27 @@ for boards 1-1 to 1-4, using my own implementation of the a*-algorithm.
 from GUI import draw_path
 
 class Cell:
-
+    """
+    One 'node' or element in the given string/board.
+    """
     def __init__(self, x, y, wall):
         self.x = x
         self.y = y
-        self.wall = wall
+        self.wall = wall        # boolean
         self.children = []
         self.parent = None
-        self.g = float('inf')  # cost for this path so far
-        self.h = None  # estimated remaining cost for this path
+        self.g = float('inf')   # g(s), cost for this path so far
+        self.h = None           # h(s), estimated remaining cost for this path
         self.solution = False
-        self.cost = 1  # can be changed for later implementations
-        # self.state = int(str(x)+str(y)) # not used
+        self.cost = 1           # can be changed for later implementations
 
     def f(self):
+        """
+        :return: Estimated total cost for a path going through this node.
+        """
         return self.g + self.h
 
+    # The following are simply used for comparison and printing purposes
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
@@ -32,6 +37,11 @@ class Cell:
 
 
 def build_children(cell, possible_cells):
+    """
+    Fills 'cell.children' with all neighbours of 'cell'
+    :param cell: Current cell
+    :param possible_cells: All cells
+    """
     for c in possible_cells:
         if (c.x == cell.x + 1 and c.y == cell.y) or\
                 (c.x == cell.x - 1 and c.y == cell.y) or\
@@ -42,6 +52,14 @@ def build_children(cell, possible_cells):
 
 
 def a_star_loop(open_nodes, closed_nodes, cells):
+    """
+    Finds a pretty optimal path, but not always the best one :))
+
+    :param open_nodes: Essentially just a list containing the starting cell, but this is populated as the algorithm runs.
+    :param closed_nodes: As above, empty when starting.
+    :param cells: All cells in the grid, used to populate the above.
+    :return: Boolean, true if the goal is found.
+    """
     done = False
     while not done:
         if not open_nodes:
@@ -52,10 +70,10 @@ def a_star_loop(open_nodes, closed_nodes, cells):
             return True
         build_children(current, cells)
         for child in current.children:
-            if child not in open_nodes and child not in closed_nodes:  # i.e have we already checked this cell
+            if child not in open_nodes and child not in closed_nodes:  # i.e 'Have we already checked this cell?'
                 attach_and_evaluate(current, child)
                 open_nodes.append(child)
-                open_nodes.sort(key=lambda x: x.h)
+                open_nodes.sort(key=lambda x: x.h)  # sort by lowest estimated cost
             elif current.g + child.cost < child.g:
                 attach_and_evaluate(current, child)
                 if child in closed_nodes:
@@ -64,19 +82,28 @@ def a_star_loop(open_nodes, closed_nodes, cells):
         return False
 
 
-def attach_and_evaluate(p, c):
-    c.parent = p
-    c.g = p.g + c.cost
-    c.h = abs(goal.x - c.x) + abs(goal.y - c.y)
+def attach_and_evaluate(parent, child):
+    child.parent = parent
+    child.g = parent.g + child.cost
+    child.h = abs(goal.x - child.x) + abs(goal.y - child.y)  # Manhattan-distance
 
 
 def propagate(cell):
+    """
+    Recursively propagate possible improvements in estimated cost and best path.
+    :param cell: Current cell
+    """
     for child in cell.children:
         if cell.g + child.cost < child.g:
             child.parent = cell
             child.g = cell.g + child.cost
             propagate(child)
 
+
+"""
+This bit simply builds the grid, iterating over the board to create all cells.
+Sets some starting parameters for the start and goal.
+"""
 board = input('filename: ')
 cells = []
 closed_nodes = []
@@ -105,6 +132,11 @@ start.h = abs(goal.x - start.x) + abs(goal.y - start.y)
 start.g = 0
 open_nodes.append(start)
 
+
+"""
+Runs the algorithm, and then traces back from goal to start using Cell.parent to find the path that was chosen.
+Prints a board with the path marked, and sends the string to Tkinter for drawing.
+"""
 if a_star_loop(open_nodes, closed_nodes, cells):
     lines = [line.strip('\n') for line in open(board, 'r')]
     cell = goal
