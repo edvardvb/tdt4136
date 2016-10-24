2e695b1773a0720bbf0d997835ac5f12f70dd3b2
Edvard Bakken <edvard.vb@live.no>
2016-10-24 17:35:19 +0200

    Add assignment 5

---
+++ assignment-5/assignment5.py
@@ -0,0 +1,276 @@
+#!/usr/bin/python
+
+import copy
+import itertools
+
+class CSP:
+    def __init__(self):
+        # self.variables is a list of the variable names in the CSP
+        self.variables = []
+
+        # self.domains[i] is a list of legal values for variable i
+        self.domains = {}
+
+        # self.constraints[i][j] is a list of legal value pairs for
+        # the variable pair (i, j)
+        self.constraints = {}
+
+        self.results = {'success': 0, 'failure': 0}
+
+    def add_variable(self, name, domain):
+        """Add a new variable to the CSP. 'name' is the variable name
+        and 'domain' is a list of the legal values for the variable.
+        """
+        self.variables.append(name)
+        self.domains[name] = list(domain)
+        self.constraints[name] = {}
+
+    def get_all_possible_pairs(self, a, b):
+        """Get a list of all possible pairs (as tuples) of the values in
+        the lists 'a' and 'b', where the first component comes from list
+        'a' and the second component comes from list 'b'.
+        """
+        return itertools.product(a, b)
+
+    def get_all_arcs(self):
+        """Get a list of all arcs/constraints that have been defined in
+        the CSP. The arcs/constraints are represented as tuples (i, j),
+        indicating a constraint between variable 'i' and 'j'.
+        """
+        return [ (i, j) for i in self.constraints for j in self.constraints[i] ]
+
+    def get_all_neighboring_arcs(self, var):
+        """Get a list of all arcs/constraints going to/from variable
+        'var'. The arcs/constraints are represented as in get_all_arcs().
+        """
+        return [ (i, var) for i in self.constraints[var] ]
+
+    def add_constraint_one_way(self, i, j, filter_function):
+        """Add a new constraint between variables 'i' and 'j'. The legal
+        values are specified by supplying a function 'filter_function',
+        that returns True for legal value pairs and False for illegal
+        value pairs. This function only adds the constraint one way,
+        from i -> j. You must ensure that the function also gets called
+        to add the constraint the other way, j -> i, as all constraints
+        are supposed to be two-way connections!
+        """
+        if not j in self.constraints[i]:
+            # First, get a list of all possible pairs of values between variables i and j
+            self.constraints[i][j] = self.get_all_possible_pairs(self.domains[i], self.domains[j])
+
+        # Next, filter this list of value pairs through the function
+        # 'filter_function', so that only the legal value pairs remain
+        self.constraints[i][j] = filter(lambda value_pair: filter_function(*value_pair), self.constraints[i][j])
+
+    def add_all_different_constraint(self, variables):
+        """Add an Alldiff constraint between all of the variables in the
+        list 'variables'.
+        """
+        for (i, j) in self.get_all_possible_pairs(variables, variables):
+            if i != j:
+                self.add_constraint_one_way(i, j, lambda x, y: x != y)
+
+    def backtracking_search(self):
+        """This functions starts the CSP solver and returns the found
+        solution.
+        """
+        # Make a so-called "deep copy" of the dictionary containing the
+        # domains of the CSP variables. The deep copy is required to
+        # ensure that any changes made to 'assignment' does not have any
+        # side effects elsewhere.
+        assignment = copy.deepcopy(self.domains)
+        # Run AC-3 on all constraints in the CSP, to weed out all of the
+        # values that are not arc-consistent to begin with
+        self.inference(assignment, self.get_all_arcs())
+
+        # Call backtrack with the partial assignment 'assignment'
+        return self.backtrack(assignment)
+
+    def backtrack(self, assignment):
+        """The function 'Backtrack' from the pseudocode in the
+        textbook.
+
+        The function is called recursively, with a partial assignment of
+        values 'assignment'. 'assignment' is a dictionary that contains
+        a list of all legal values for the variables that have *not* yet
+        been decided, and a list of only a single value for the
+        variables that *have* been decided.
+
+        When all of the variables in 'assignment' have lists of length
+        one, i.e. when all variables have been assigned a value, the
+        function should return 'assignment'. Otherwise, the search
+        should continue. When the function 'inference' is called to run
+        the AC-3 algorithm, the lists of legal values in 'assignment'
+        should get reduced as AC-3 discovers illegal values.
+
+        IMPORTANT: For every iteration of the for-loop in the
+        pseudocode, you need to make a deep copy of 'assignment' into a
+        new variable before changing it. Every iteration of the for-loop
+        should have a clean slate and not see any traces of the old
+        assignments and inferences that took place in previous
+        iterations of the loop.
+        """
+        self.results['success'] += 1
+
+        """
+        Remove domains for variables with only one value(aka variables that are already set), to check if we're done.
+        """
+        partial_domain = [e for e in assignment.values() if len(e) != 1]
+        if not partial_domain:
+            return assignment
+
+
+        var = self.select_unassigned_variable(assignment)
+
+        # Iterates through all potential values for the selected var.
+        for val in assignment[var]:
+            # Set val as the only value for var in the copy of assignment.
+            a_copy = copy.deepcopy(assignment)
+            a_copy[var] = [val]
+
+            """
+            Run AC3 to check the value for consistency. If it's consistent we go deeper into the recursion
+            by calling backtrack with the copy of assignment. When all variable-domains have length = 1,
+            'success' will evaluate as True, and we can return the assignment stored in 'success'.
+
+            If it isn't consistent, self.inference will return False, and we try a new value.
+            """
+            if self.inference(a_copy, self.get_all_arcs()):
+                success = self.backtrack(a_copy)
+                if success:
+                    return success
+
+        self.results['failure'] += 1
+        return False
+
+
+
+    def select_unassigned_variable(self, assignment):
+        """The function 'Select-Unassigned-Variable' from the pseudocode
+        in the textbook. Should return the name of one of the variables
+        in 'assignment' that have not yet been decided, i.e. whose list
+        of legal values has a length greater than one.
+        """
+
+        # Selects the first unassigned variable
+        for i in assignment:
+            if len(assignment[i])>1:
+                return i
+
+    def inference(self, assignment, queue):
+        """The function 'AC-3' from the pseudocode in the textbook.
+        'assignment' is the current partial assignment, that contains
+        the lists of legal values for each undecided variable. 'queue'
+        is the initial queue of arcs that should be visited.
+        """
+
+        # If we get through the entire queue without returning false, we have consistency
+        while queue:
+            # Pop a tuple containing two variables
+            arc = queue.pop()
+
+            # Check the variables in arc for consistency
+            if self.revise(assignment, *arc):
+                # If a variable has no domain, the current state is not consistent.
+                if not assignment[arc[0]]:
+                    return False
+                # Pull all neighbours of arc[0] (remove duplicates etc.) and append them to the queue
+                for e in self.get_all_neighboring_arcs(arc[0]):
+                    if e[0] not in arc:
+                        queue.append((e[0], arc[0]))
+        return True
+
+    def revise(self, assignment, i, j):
+        """The function 'Revise' from the pseudocode in the textbook.
+        'assignment' is the current partial assignment, that contains
+        the lists of legal values for each undecided variable. 'i' and
+        'j' specifies the arc that should be visited. If a value is
+        found in variable i's domain that doesn't satisfy the constraint
+        between i and j, the value should be deleted from i's list of
+        legal values in 'assignment'.
+        """
+
+        # Pull all constraints for these variables
+        cons = list(self.constraints[i][j])
+        revised = False
+
+        # Double iteration over values in i and j's domains.
+        for val_i in assignment[i]:
+            to_remove = True
+            for val_j in assignment[j]:
+                # If a set of values matching the constraints is found, val_i will not be removed.
+                if (val_i, val_j) in cons:
+                    to_remove = False
+            # If no matching value-sets were found, remove val_i from i's domain and return True.
+            if to_remove:
+                assignment[i].remove(val_i)
+                revised = True
+        return revised
+
+def create_map_coloring_csp():
+    """Instantiate a CSP representing the map coloring problem from the
+    textbook. This can be useful for testing your CSP solver as you
+    develop your code.
+    """
+    csp = CSP()
+    states = [ 'WA', 'NT', 'Q', 'NSW', 'V', 'SA', 'T' ]
+    edges = { 'SA': [ 'WA', 'NT', 'Q', 'NSW', 'V' ], 'NT': [ 'WA', 'Q' ], 'NSW': [ 'Q', 'V' ] }
+    colors = [ 'red', 'green', 'blue' ]
+    for state in states:
+        csp.add_variable(state, colors)
+    for state, other_states in edges.items():
+        for other_state in other_states:
+            csp.add_constraint_one_way(state, other_state, lambda i, j: i != j)
+            csp.add_constraint_one_way(other_state, state, lambda i, j: i != j)
+    return csp
+
+def create_sudoku_csp(filename):
+    """Instantiate a CSP representing the Sudoku board found in the text
+    file named 'filename' in the current directory.
+    """
+    csp = CSP()
+    board = map(lambda x: x.strip(), open(filename, 'r'))
+
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == '0':
+                csp.add_variable('%d-%d' % (row, col), map(str, range(1, 10)))
+            else:
+                csp.add_variable('%d-%d' % (row, col), [ board[row][col] ])
+
+    for row in range(9):
+        csp.add_all_different_constraint([ '%d-%d' % (row, col) for col in range(9) ])
+    for col in range(9):
+        csp.add_all_different_constraint([ '%d-%d' % (row, col) for row in range(9) ])
+    for box_row in range(3):
+        for box_col in range(3):
+            cells = []
+            for row in range(box_row * 3, (box_row + 1) * 3):
+                for col in range(box_col * 3, (box_col + 1) * 3):
+                    cells.append('%d-%d' % (row, col))
+            csp.add_all_different_constraint(cells)
+
+    return csp
+
+def print_sudoku_solution(solution):
+    """Convert the representation of a Sudoku solution as returned from
+    the method CSP.backtracking_search(), into a human readable
+    representation.
+    """
+    for row in range(9):
+        for col in range(9):
+            print solution['%d-%d' % (row, col)][0],
+            if col == 2 or col == 5:
+                print '|',
+        print
+        if row == 2 or row == 5:
+            print '------+-------+------'
+
+
+boards = ['sudoku-boards/easy.txt','sudoku-boards/medium.txt','sudoku-boards/hard.txt', 'sudoku-boards/veryhard.txt']
+b = input('Select board 0-3: ')
+csp = create_sudoku_csp(boards[b])
+print_sudoku_solution(csp.backtracking_search())
+print '\n'
+print 'Total backtracks: ' + str(csp.results['success']+csp.results['failure'])
+print 'Failed backtracks: ' + str(csp.results['failure'])
